5798f9fca3a2e7e89e0491950762f1f10788fb07
Serzhan Akhmetov <siaarzh@gmail.com>
2018-12-01 21:44:49 +0600

    End of Section 07-077:
    - Created test_register_and_login system test
    - Created test_register_duplicate_user system test

--- tests/system/test_user.py
+++ tests/system/test_user.py
@@ -7,18 +7,44 @@ class UserTest(BaseTest):
     def test_register_user(self):
         with self.app() as client:  # allows method to act as ext. client
             with self.app_context():
-                request = client.post('/register', data={
+                response = client.post('/register', data={
                     'username': 'test',
                     'password': '1234'
                 })
 
-                self.assertEqual(request.status_code, 201)
+                self.assertEqual(response.status_code, 201)
                 self.assertIsNotNone(UserModel.find_by_username('test'))
                 self.assertDictEqual({'message': 'User created successfully.'},
-                                     json.loads(request.data))
+                                     json.loads(response.data))
 
     def test_register_and_login(self):
-        pass
+        with self.app() as client:  # allows method to act as ext. client
+            with self.app_context():
+                response = client.post('/register', data={
+                    'username': 'test',
+                    'password': '1234'
+                })
+                auth_request = client.post('/auth',
+                                           data=json.dumps({
+                                               'username': 'test',
+                                               'password': '1234'
+                                           }),
+                                           headers={'Content-Type': 'application/json'})
+
+                self.assertIn('access_token', json.loads(auth_request.data).keys())  # ['access_token']
 
     def test_register_duplicate_user(self):
-        pass
+        with self.app() as client:  # allows method to act as ext. client
+            with self.app_context():
+                client.post('/register', data={
+                    'username': 'test',
+                    'password': '1234'
+                })
+                response = client.post('/register', data={
+                    'username': 'test',
+                    'password': '1234'
+                })
+
+                self.assertEqual(response.status_code, 400)
+                self.assertDictEqual({'message': 'A user with that username already exists'},
+                                     json.loads(response.data))

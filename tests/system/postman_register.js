ce1c77676f5bcb24a210c048f318b6e5e2d39fa7
Serzhan Akhmetov <siaarzh@gmail.com>
2018-12-02 23:24:58 +0600

    End of Section 08-084:
    - Added postman /register system tests

---
+++ tests/system/postman_register.js
@@ -0,0 +1,7 @@
+tests["Response time is less than 200ms"] = responseTime < 200;
+
+var jsonData = JSON.parse(responseBody);
+
+tests["User created successfully"] = jsonData.message === 'User created successfully.';
+tests["Content-Type is present in response"] = postman.getResponseHeader('Content-Type');
+tests["Content-Type is 'application/json'"] = postman.getResponseHeader('Content-Type') === 'application/json';
